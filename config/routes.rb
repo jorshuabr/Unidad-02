24ecf3c033fcd4dc1b7d6c6bc88d4b6f11b18f2e
CodePicnic Exporter <gitexporter@codepicnic.com>
2016-11-27 21:17:42 +0000

    First commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
   #get 'static/about'
-  get 'about', controller: :static, action: :about, alias: 'about'
+  get '1', controller: :static, action: :about, alias: '2'
   
   #get 'static/services'
-  get 'services', controller: :static, action: :services, alias:'services'
-  #get 'home/index'
+  get '2', controller: :static, action: :services, alias:'1'
+
+   
+  get 'pages/1'
+
+  get 'pages/2'
+    #get 'home/index'
    root 'home#index'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
